eac020219cb71655aef9786a48aed19aeede94b5
Akash40344 <akashkhadka9851@gmail.com>
2025-12-31 06:51:50 +0545

    main origin

---
+++ wp-content/themes/learnwp-theme/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function learn_wp_styles()
+{
+    wp_enqueue_style('learnwp-style', get_stylesheet_uri(), [], '1.0.0');
+    wp_enqueue_style('tailwind', get_template_directory_uri() . '/src/output.css', [], time());
+}
+
+add_action('wp_enqueue_scripts', 'learn_wp_styles');
+
+
+add_theme_support('post-thumbnails');
